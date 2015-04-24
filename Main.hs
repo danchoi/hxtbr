6dee30de886bd5edf13d7b87a307223d314d4312
Daniel Choi <dhchoi@gmail.com>
2015-04-24 12:24:06 -0400

    test the inverse case

--- Main.hs
+++ Main.hs
@@ -10,11 +10,7 @@ import Text.XML.HXT.DOM.TypeDefs
 import Numeric
 import Data.String.Utils (replace)
 
-
-brOrP = isElem
-      >>> hasNameWith (\q -> localPart q `elem` ["br", "p"])
-
--- removes BR tags that have P tag siblings
+-- removes BR tags that have any P tag following-siblings
 stripInterstitialBR = 
         none
         `when`
